b9ef774d4dfc27a46d7b615b7c574b39526e939e
clienterr1 <clienterr@clienterr.com>
2024-05-23 00:45:59 +0545

    execution

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config;
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { DISCORD_TOKEN: token } = process.env;
+
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+// Create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+//load the events files on startup
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+//load the command files on startup
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute propert"`
+    );
+  }
+}
+
+client.login(token);
