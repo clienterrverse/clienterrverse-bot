598e416bbef4bbe25f2a608d5b7ca6963102ffd0
NorySight <114584253+GrishMahat@users.noreply.github.com>
2024-05-28 14:52:51 +0545

    Update guild-join.js

--- src/commands/misc/guild-join.js
+++ src/commands/misc/guild-join.js
@@ -1,28 +1,24 @@
 import { SlashCommandBuilder, StringSelectMenuBuilder, ActionRowBuilder, EmbedBuilder, ButtonBuilder } from 'discord.js';
-
 import { formatDistanceToNow } from 'date-fns';
 
-// Export the module to be used elsewhere
 export default {
-    // Slash command data
     data: new SlashCommandBuilder()
-        .setName('guild-join') 
-        .setDescription('join hypixel guild'), 
-
+        .setName('guild-join')
+        .setDescription('Join Hypixel guild'),
+    
     userPermissions: [],
-    bot: [],
-    cooldown: 5, 
-    nwfwMode: false,
+    botPermissions: [],
+    cooldown: 5,
+    nsfwMode: false,
     testMode: true,
     devOnly: true,
 
-
     run: async (client, interaction) => {
-
-
-
-
-      
-        await interaction.reply('Workin on it ');
+        try {
+            await interaction.reply('Working on it...');
+        } catch (error) {
+            console.error('Error handling guild-join command:', error);
+            await interaction.reply({ content: 'An error occurred while processing your request.', ephemeral: true });
+        }
     }
 };
