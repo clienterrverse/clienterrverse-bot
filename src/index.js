d0335b50673196c665fe79768809add4d75c4433
NorySight <114584253+GrishMahat@users.noreply.github.com>
2024-05-28 14:49:59 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { Client, GatewayIntentBits } from "discord.js";
+import errorHandler from './utils/errorHandler.js';
 
 if (!process.env.TOKEN) {
   console.error("TOKEN is not defined in the environment variables");
@@ -7,7 +8,7 @@ if (!process.env.TOKEN) {
 }
 
 (async () => {
-  const { default: eventHandler } = await import("./handlers/eventHandler.js");
+  const { default: eventHandler } = await import('./handlers/eventHandler.js');
 
   const client = new Client({
     intents: [
@@ -16,16 +17,20 @@ if (!process.env.TOKEN) {
       GatewayIntentBits.GuildMessages,
       GatewayIntentBits.MessageContent,
       GatewayIntentBits.DirectMessages,
-      GatewayIntentBits.GuildMessageReactions,
-      GatewayIntentBits.GuildInvites,
     ],
   });
+  errorHandler.errorHandler(client);
   await eventHandler(client);
 
   try {
     await client.login(process.env.TOKEN);
   } catch (error) {
-    console.error("Error logging in:", error);
+    console.error('Error logging in:', error);
     process.exit(1);
   }
+    // Schedule daily error summary report
+    setInterval(() => {
+      errorHandler.sendDailyErrorSummaryReport(errorHandler.errorCounts);
+    }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+  
 })();
