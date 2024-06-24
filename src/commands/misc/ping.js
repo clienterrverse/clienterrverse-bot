7d6bc5cb28ce2176b016de462e8b6e1e9c5e8610
NorySight <114584253+GrishMahat@users.noreply.github.com>
2024-06-25 00:10:54 +0545

    Update ping.js

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -16,45 +16,50 @@ export default {
     devOnly: false,
 
     run: async (client, interaction) => {
-        if (interaction.replied || interaction.deferred) {
-            return;
+        try {
+            if (interaction.replied || interaction.deferred) {
+                console.error('Interaction has already been acknowledged.');
+                return;
+            }
+
+            let pingColor = '';
+            const ping = interaction.client.ws.ping;
+
+            if (ping < 150) {
+                pingColor = '#00ff00'; // Green
+            } else if (ping >= 150 && ping <= 250) {
+                pingColor = '#ffff00'; // Yellow
+            } else {
+                pingColor = '#ff0000'; // Red
+            }
+
+            const uptime = formatDistanceToNow(client.readyAt, { includeSeconds: true });
+
+            if (!client.commandStats) {
+                client.commandStats = {
+                    ping: 0,
+                    totalPing: 0
+                };
+            }
+
+            client.commandStats.ping += 1;
+            client.commandStats.totalPing += ping;
+
+            const averagePing = client.commandStats.totalPing / client.commandStats.ping;
+
+            const pongEmbed = new EmbedBuilder()
+                .setColor(pingColor) 
+                .setTitle('Pong') 
+                .setDescription(`**Ping:** ${ping} ms\n**Average Ping:** ${averagePing.toFixed(2)} ms\n**Uptime:** ${uptime}\n**Command Usage:** ${client.commandStats.ping}`)
+                .setFooter({
+                    text: `Requested by ${interaction.user.username}`,
+                    iconURL: `${interaction.user.displayAvatarURL({ dynamic: true })}`,
+                })
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [pongEmbed] });
+        } catch (error) {
+            console.error('An error occurred while processing command:', error.message);
         }
-
-        let pingColor = '';
-        const ping = interaction.client.ws.ping;
-
-        if (ping < 150) {
-            pingColor = '#00ff00'; // Green
-        } else if (ping >= 150 && ping <= 250) {
-            pingColor = '#ffff00'; // Yellow
-        } else {
-            pingColor = '#ff0000'; // Red
-        }
-
-        const uptime = formatDistanceToNow(client.readyAt, { includeSeconds: true });
-
-        if (!client.commandStats) {
-            client.commandStats = {
-                ping: 0,
-                totalPing: 0
-            };
-        }
-
-        client.commandStats.ping += 1;
-        client.commandStats.totalPing += ping;
-
-        const averagePing = client.commandStats.totalPing / client.commandStats.ping;
-
-        const pongEmbed = new EmbedBuilder()
-            .setColor(pingColor) 
-            .setTitle('Pong') 
-            .setDescription(`**Ping:** ${ping} ms\n**Average Ping:** ${averagePing.toFixed(2)} ms\n**Uptime:** ${uptime}\n**Command Usage:** ${client.commandStats.ping}`)
-            .setFooter({
-                text: `Requested by ${interaction.user.username}`,
-                iconURL: `${interaction.user.displayAvatarURL({ dynamic: true })}`,
-            })
-            .setTimestamp();
-
-        await interaction.reply({ embeds: [pongEmbed] });
     }
 };
