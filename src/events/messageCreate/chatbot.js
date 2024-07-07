d045de2e754e1f1d3e2bb1ab270ae65e8906758c
NorySight <114584253+GrishMahat@users.noreply.github.com>
2024-07-07 12:50:37 +0000

    fix Not connecting to db

--- src/events/messageCreate/chatbot.js
+++ src/events/messageCreate/chatbot.js
@@ -1,7 +1,3 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const MODEL_NAME = 'gemini-pro';
 const allowedChannelIds = ['1244565914213679124'];
 
 export default async (client, message) => {
