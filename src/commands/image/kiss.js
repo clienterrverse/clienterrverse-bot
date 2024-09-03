c5f0d5e7d4ab811a15690f039f13e3f241215e4e
NorySight <114584253+GrishMahat@users.noreply.github.com>
2024-09-03 08:13:15 +0000

    Update discordjs to 14.16

--- src/commands/image/kiss.js
+++ src/commands/image/kiss.js
@@ -18,12 +18,16 @@ export default {
             .setName('user2')
             .setDescription('Second user whose avatar you want to use')
             .setRequired(true)
-      ),
+      )
+      .setContexts([0, 1, 2])
+      .setIntegrationTypes([0, 1])
+      .toJSON(),
    category: 'Image',
    nwfwMode: false,
    testMode: false,
    devOnly: false,
    prefix: true,
+   dmAllowed: true,
 
    userPermissionsBitField: [],
    bot: [],
